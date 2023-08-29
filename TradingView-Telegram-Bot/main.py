93e0ec103e474d3110a0bb622f36ac21065e1cde
Zeeshan Ahmad <ahmadzee26@gmail.com>
2023-08-29 17:20:15 +0500

    Uploading Whole Project

--- TradingView-Telegram-Bot/main.py
+++ TradingView-Telegram-Bot/main.py
@@ -1,1 +1,11 @@
  
+# main.py
+from services import tradingview, telegram
+from config import settings
+
+def main():
+    # Placeholder for main logic
+    pass
+
+if __name__ == "__main__":
+    main()
